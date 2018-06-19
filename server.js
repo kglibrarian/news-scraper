891f0803046d0fe26ddaa9b74081185ff14d5efd
Karen E Gutzman <keg827@fsm.northwestern.edu>
2018-06-18 20:54:31 -0500

    added port stuff

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var connection = require("./config/connection.js");
 // var cheerio = require("cheerio");
 
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
