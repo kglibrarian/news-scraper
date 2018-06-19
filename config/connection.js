2c0d326f5608158b7d65d07bcac35a94df65c9b8
Karen E Gutzman <keg827@fsm.northwestern.edu>
2018-06-18 21:05:20 -0500

    added database variables

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,25 @@ module.exports = function(app) {
 var mongoose = require("mongoose");
 
 //Set up default mongoose connection
-var mongoDB = 'mongodb://localhost/news-scraper';
-mongoose.connect(mongoDB);
+// var mongoDB = 'mongodb://localhost/news-scraper';
+// mongoose.connect(mongoDB);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 // Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 //Get the default connection
-var db = mongoose.connection;
+// var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 };
 // Export connection for our ORM to use.
